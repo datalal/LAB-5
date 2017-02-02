7b0686148ddd0dbe220dca04e27d82f1fdeab018
datalal <lawsonw2@gmail.com>
2017-02-02 16:05:27 -0500

    fizz buzz more beautify
    fizzbuzzz

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,12 +1,10 @@
 for (var i = 1; i < 101; i++) {
     if (i % 5 === 0 && i % 3 === 0) {
         console.log("fizzBuzz");
-  } else if (i % 5 === 0)
-    {
+    } else if (i % 5 === 0) {
         console.log("Buzz");
-  } else if (i % 3 === 0)
-    {
+    } else if (i % 3 === 0) {
         console.log("fizz");
-  } else
+    } else
         console.log(i);
 }
