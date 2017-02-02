d987f8f40357d5f00f014b05a55e59985a359fae
datalal <lawsonw2@gmail.com>
2017-02-02 16:02:12 -0500

    fizz buzz bonus
    FizzBuzz

---
+++ fizzbuzz.js
@@ -0,0 +1,23 @@
+for (var i = 1; i < 101; i++) {
+
+    if (i % 5 === 0 && i % 3 === 0){
+
+      console.log("fizzBuzz");
+
+
+    } else if (i % 5 === 0)
+
+      {
+        console.log("Buzz");
+
+      } else if (i % 3 === 0)
+
+        {
+
+          console.log("fizz");
+
+        } else
+
+
+        console.log(i);
+}
