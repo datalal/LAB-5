ec989f7b88df31b74716ecc9ff4fd66136bab22e
datalal <lawsonw2@gmail.com>
2017-02-03 09:29:44 -0500

    radio selection update
    FtoC and CtoF

--- converter.js
+++ converter.js
@@ -1,18 +1,43 @@
 function Convert() {
 
     //var tempSelect = document.getElementById("convertSelect").value;
-    var Far = document.getElementById("inputTemp").value;
+    var inputTemp1 = document.getElementById("inputTemp").value;
 
-    var Cel = ((Far - 32) * 5) / 9;
+    var Cel = ((inputTemp1 - 32) * 5) / 9;
+    var Far = ((inputTemp1 * 9) / 5) + 32;
 
-    if (Far > 60) {
 
-        document.getElementById("convertResult").innerHTML = "The current temperature in Celsius is: " + Cel + " °C" + " It's HOT out!!";
-        document.getElementById("dogTemp").innerHTML = "<img class='dogPic' src='hotdog.jpg'>"
-    } else {
 
-        document.getElementById("convertResult").innerHTML = "The current temperature in Celsius is: " + Cel + " °C" + " It's COOOOLD outside!!";
-        document.getElementById("dogTemp").innerHTML = "<img class='dogPic' src='colddog.jpg'>"
+    if(document.getElementById('convertSelect1').checked) {
 
-    }
+      if (Cel > 16) {
+
+          document.getElementById("convertResult").innerHTML = "The current temperature in Celsius is: " + Cel + " °C" + " It's HOT out!!";
+          document.getElementById("dogTemp").innerHTML = "<img class='dogPic' src='hotdog.jpg'>"
+      } else {
+
+          document.getElementById("convertResult").innerHTML = "The current temperature in Celsius is: " + Cel + " °C" + " It's COOOOLD outside!!";
+          document.getElementById("dogTemp").innerHTML = "<img class='dogPic' src='colddog.jpg'>"
+
+      }
+
+
+     } else if (document.getElementById('convertSelect2').checked) {
+
+       if (Far > 60) {
+
+           document.getElementById("convertResult").innerHTML = "The current temperature in Fahrenheit is: " + Far + " °F" + " It's HOT out!!";
+           document.getElementById("dogTemp").innerHTML = "<img class='dogPic' src='hotdog.jpg'>"
+       } else {
+
+           document.getElementById("convertResult").innerHTML = "The current temperature in Fahrenheit is: " + Far + " °F" + " It's COOOOLD outside!!";
+           document.getElementById("dogTemp").innerHTML = "<img class='dogPic' src='colddog.jpg'>"
+
+       }
+
+
+
+     }
+
+
 }
