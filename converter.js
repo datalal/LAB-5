b69fdb80edfda1bb475de3b341d3bfc7505c139c
datalal <lawsonw2@gmail.com>
2017-02-06 14:34:36 -0500

    function update
    get value of selected radio button

--- converter.js
+++ converter.js
@@ -1,39 +1,41 @@
 function newConvert(temp, unit){
 
   var temp = document.getElementById("inputTemp").value;
-  var unit = document.getElementById('convertSelect1').value;
+  var unit = document.querySelector('input[name="convertSelect"]:checked').value;
 
 
-
-    var Cel = ((temp - 32) * 5) / 9;
-    var Far = ((temp * 9) / 5) + 32;
-
-
+  var Cel = ((temp - 32) * 5) / 9;
+  var Far = ((temp * 9) / 5) + 32;
 
     if (unit === "C") {
+
+
 
         if (Cel > 16) {
 
             document.getElementById("convertResult").innerHTML = "The current temperature in Celsius is: " + Cel + " °C" + " It's HOT out!!";
-            document.getElementById("dogTemp").innerHTML = "<img class='dogPic' src='hotdog.jpg'>"
+            document.getElementById("dogTemp").innerHTML = "<img class='dogPic' src='hotdog.jpg'>";
+
         } else {
 
             document.getElementById("convertResult").innerHTML = "The current temperature in Celsius is: " + Cel + " °C" + " It's COOOOLD outside!!";
-            document.getElementById("dogTemp").innerHTML = "<img class='dogPic' src='colddog.jpg'>"
+            document.getElementById("dogTemp").innerHTML = "<img class='dogPic' src='colddog.jpg'>";
 
         }
 
 
     } else if (unit === "F") {
 
+
+
             if (Far > 60) {
 
                 document.getElementById("convertResult").innerHTML = "The current temperature in Fahrenheit is: " + Far + " °F" + " It's HOT out!!";
-                document.getElementById("dogTemp").innerHTML = "<img class='dogPic' src='hotdog.jpg'>"
+                document.getElementById("dogTemp").innerHTML = "<img class='dogPic' src='hotdog.jpg'>";
             } else {
 
                 document.getElementById("convertResult").innerHTML = "The current temperature in Fahrenheit is: " + Far + " °F" + " It's COOOOLD outside!!";
-                document.getElementById("dogTemp").innerHTML = "<img class='dogPic' src='colddog.jpg'>"
+                document.getElementById("dogTemp").innerHTML = "<img class='dogPic' src='colddog.jpg'>";
 
             }
 
@@ -46,47 +48,47 @@ function newConvert(temp, unit){
 
 
 
-
-function Convert() {
-
-    //var tempSelect = document.getElementById("convertSelect").value;
-    var inputTemp1 = document.getElementById("inputTemp").value;
-
-    var Cel = ((inputTemp1 - 32) * 5) / 9;
-    var Far = ((inputTemp1 * 9) / 5) + 32;
-
-
-
-    if(document.getElementById('convertSelect1').checked) {
-
-      if (Cel > 16) {
-
-          document.getElementById("convertResult").innerHTML = "The current temperature in Celsius is: " + Cel + " °C" + " It's HOT out!!";
-          document.getElementById("dogTemp").innerHTML = "<img class='dogPic' src='hotdog.jpg'>"
-      } else {
-
-          document.getElementById("convertResult").innerHTML = "The current temperature in Celsius is: " + Cel + " °C" + " It's COOOOLD outside!!";
-          document.getElementById("dogTemp").innerHTML = "<img class='dogPic' src='colddog.jpg'>"
-
-      }
-
-
-     } else if (document.getElementById('convertSelect2').checked) {
-
-       if (Far > 60) {
-
-           document.getElementById("convertResult").innerHTML = "The current temperature in Fahrenheit is: " + Far + " °F" + " It's HOT out!!";
-           document.getElementById("dogTemp").innerHTML = "<img class='dogPic' src='hotdog.jpg'>"
-       } else {
-
-           document.getElementById("convertResult").innerHTML = "The current temperature in Fahrenheit is: " + Far + " °F" + " It's COOOOLD outside!!";
-           document.getElementById("dogTemp").innerHTML = "<img class='dogPic' src='colddog.jpg'>"
-
-       }
-
-
-
-     }
-
-
-}
+//
+// function Convert() {
+//
+//     //var tempSelect = document.getElementById("convertSelect").value;
+//     var inputTemp1 = document.getElementById("inputTemp").value;
+//
+//     var Cel = ((inputTemp1 - 32) * 5) / 9;
+//     var Far = ((inputTemp1 * 9) / 5) + 32;
+//
+//
+//
+//     if(document.getElementById('convertSelect1').checked) {
+//
+//       if (Cel > 16) {
+//
+//           document.getElementById("convertResult").innerHTML = "The current temperature in Celsius is: " + Cel + " °C" + " It's HOT out!!";
+//           document.getElementById("dogTemp").innerHTML = "<img class='dogPic' src='hotdog.jpg'>";
+//       } else {
+//
+//           document.getElementById("convertResult").innerHTML = "The current temperature in Celsius is: " + Cel + " °C" + " It's COOOOLD outside!!";
+//           document.getElementById("dogTemp").innerHTML = "<img class='dogPic' src='colddog.jpg'>";
+//
+//       }
+//
+//
+//      } else if (document.getElementById('convertSelect2').checked) {
+//
+//        if (Far > 60) {
+//
+//            document.getElementById("convertResult").innerHTML = "The current temperature in Fahrenheit is: " + Far + " °F" + " It's HOT out!!";
+//            document.getElementById("dogTemp").innerHTML = "<img class='dogPic' src='hotdog.jpg'>";
+//        } else {
+//
+//            document.getElementById("convertResult").innerHTML = "The current temperature in Fahrenheit is: " + Far + " °F" + " It's COOOOLD outside!!";
+//            document.getElementById("dogTemp").innerHTML = "<img class='dogPic' src='colddog.jpg'>";
+//
+//        }
+//
+//
+//
+//      }
+//
+//
+// }
