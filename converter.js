0aff104b078ec645d975daa79810ddc0387b5e76
datalal <lawsonw2@gmail.com>
2017-02-06 12:06:06 -0500

    function update
    update with a function to take unit and temp

--- converter.js
+++ converter.js
@@ -1,3 +1,52 @@
+function newConvert(temp, unit){
+
+  var temp = document.getElementById("inputTemp").value;
+  var unit = document.getElementById('convertSelect1').value;
+
+
+
+    var Cel = ((temp - 32) * 5) / 9;
+    var Far = ((temp * 9) / 5) + 32;
+
+
+
+    if (unit === "C") {
+
+        if (Cel > 16) {
+
+            document.getElementById("convertResult").innerHTML = "The current temperature in Celsius is: " + Cel + " °C" + " It's HOT out!!";
+            document.getElementById("dogTemp").innerHTML = "<img class='dogPic' src='hotdog.jpg'>"
+        } else {
+
+            document.getElementById("convertResult").innerHTML = "The current temperature in Celsius is: " + Cel + " °C" + " It's COOOOLD outside!!";
+            document.getElementById("dogTemp").innerHTML = "<img class='dogPic' src='colddog.jpg'>"
+
+        }
+
+
+    } else if (unit === "F") {
+
+            if (Far > 60) {
+
+                document.getElementById("convertResult").innerHTML = "The current temperature in Fahrenheit is: " + Far + " °F" + " It's HOT out!!";
+                document.getElementById("dogTemp").innerHTML = "<img class='dogPic' src='hotdog.jpg'>"
+            } else {
+
+                document.getElementById("convertResult").innerHTML = "The current temperature in Fahrenheit is: " + Far + " °F" + " It's COOOOLD outside!!";
+                document.getElementById("dogTemp").innerHTML = "<img class='dogPic' src='colddog.jpg'>"
+
+            }
+
+
+
+        }
+
+   }
+
+
+
+
+
 function Convert() {
 
     //var tempSelect = document.getElementById("convertSelect").value;
